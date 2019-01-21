5fbb4e64e547eeddc39bc3e0c870b2f274b0523c
Camila <barbosacamila97@gmail.com>
2019-01-21 01:11:21 -0300

    O restante do trabalho, falta umas coisas ainda

--- 1.cpp
+++ 1.cpp
@@ -3,37 +3,89 @@
 #define WIDTH	500
 #define HEIGHT	500
 
-
+//Primeiro desenho (triagulo) (100%)//
 void triangulo(){
 	glColor3d(1.0d, 1.0d, 1.0d);
 
 	glBegin(GL_TRIANGLES);
-		glVertex2i(0, 100);
-		glVertex2i(300, 200);
-		glVertex2i(100, 250);
+		glVertex2d(1, 250);
+		glVertex2d(499, 250);
+		glVertex2d(250, 499);
+
 	glEnd();
 }
 
+//Segundo desenho (quadrado, mas que na verdade sao dois triangulos)//
+//A cor tá meio bugada//
+void pseudoquadrado(){
+    glBegin(GL_TRIANGLE_STRIP);
+
+        glColor3d(1.0d, 1.0d, 1.0d);
+        glVertex2d(1, 250);
+        glVertex2d(499, 250);
+        glVertex2d(250, 499);
+
+        glColor3d(0.0d, 0.0d, 1.0d);
+        glVertex2d(1, 250);
+        glVertex2d(499, 250);
+        glVertex2d(250, 1);
+
+    glEnd();
+}
+
+//void duaslinhas(){
+    glBegin(GL_LINE_STRIP); //Eu acho que essas coordenadas estao certa, pelo amor de deus??????
+
+    glColor3d(1.0d, 1.0d, 1.0d);
+    glVertex2d(120, 300);
+    glColor3d(0.0d, 0.0d, 0.0d);
+    glVertex2d(300, 350);
+    glVertex2d(250,1);
+
+    glEnd();
+}
+
+void acolorfulsquare(){
+    glBegin(GL_QUADS);
+    //fica faltando um pedaco do triangulo help
+    glColor3d(0.0d, 0.0d, 1.0d);
+    glVertex2d(250, 499);
+    glColor3d(0.0d, 1.0d, 0.0);
+    glVertex2d(499, 499);
+    glColor3d(1.0d, 0.0d, 0.0d);
+    glVertex2d(250, 250);
+    glColor3d(1.0d, 1.0d, 1.0d);
+    glVertex2d(499, 250);
+
+    glEnd();
+}
 void desenha(){
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	glViewport(250, 250, 250, 250);
+    //Arrumar as viewports e provavelmente os valores de todas as coordenadas
+	glViewport(0, 0, 500, 500);
 	triangulo();
+	glViewport(0, 0, 500, 500);
+	pseudoquadrado();
+	glViewport(0, 0, 500, 500);
+	duaslinhas();
+	glViewport(0, 0, 500, 500);
+	acolorfulsquare();
 
 	glFlush();
 }
 
 void start(){
-	glClearColor(1.0d, 0.0d, 0.0d, 0.0d);
+	//Como deixar cada janelinha com a cor própria?
+	glClearColor(1.0d, 1.0d, 1.0d, 1.0d);
 	glOrtho(0, WIDTH, 0, HEIGHT, -1, 1);
 }
-
+//Main function //
 int main(int argc, char **argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB);
 	glutInitWindowSize(WIDTH, HEIGHT);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("feel like dying");
+	glutCreateWindow("I just wanna go back to 1999");
 	glutDisplayFunc(desenha);
 	start();
 	glutMainLoop();

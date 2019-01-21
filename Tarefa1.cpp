62aaaf9a6dd3d7f3e66891b7331db74cf56f0579
Camila <barbosacamila97@gmail.com>
2019-01-21 16:59:18 -0300

    Finally everything done

--- Tarefa1.cpp
+++ Tarefa1.cpp
@@ -2,64 +2,67 @@
 
 #define WIDTH	500
 #define HEIGHT	500
-//Como usar a glScissor();
-//Primeiro desenho (triagulo) (100%)//
+
+
+
 void triangulo(){
+    //Desenho 1
 	glColor3d(1.0d, 1.0d, 1.0d);
 
 	glBegin(GL_TRIANGLES);
-		glVertex2d(1, 250);
-		glVertex2d(499, 250);
-		glVertex2d(250, 499);
+		glVertex2d(0, 250);
+		glVertex2d(500, 250);
+		glVertex2d(250, 500);
 
 	glEnd();
 }
 
-//Segundo desenho (quadrado, mas que na verdade sao dois triangulos)//
-//A cor tá meio bugada//
+
 void pseudoquadrado(){
+    //Desenho 2
     glBegin(GL_TRIANGLE_STRIP);
 
         glColor3d(1.0d, 1.0d, 1.0d);
-        glVertex2d(1, 250);
-        glVertex2d(499, 250);
-        glVertex2d(250, 499);
-
+        glVertex2d(0, 250);
+        glVertex2d(500, 250);
+        glVertex2d(250, 500);
+        glVertex2d(0, 250);
+        glVertex2d(500, 250);
         glColor3d(0.0d, 0.0d, 1.0d);
-        glVertex2d(1, 250);
-        glVertex2d(499, 250);
-        glVertex2d(250, 1);
+        glVertex2d(250, 0);
 
     glEnd();
 }
 
 void duaslinhas(){
-    glBegin(GL_LINE_STRIP); //Eu acho que essas coordenadas estao certa, pelo amor de deus??????
+    //Eu acho que essas coordenadas estao certa, pelo amor de deus??????
+    glBegin(GL_LINE_STRIP);
 
     glColor3d(1.0d, 1.0d, 1.0d);
-    glVertex2d(120, 300);
+    glVertex2d(150, 250);
     glColor3d(0.0d, 0.0d, 0.0d);
-    glVertex2d(300, 350);
-    glVertex2d(250,1);
+    glVertex2d(400, 400);
+    glVertex2d(250,0);
 
     glEnd();
 }
 
 void acolorfulsquare(){
+    //Desenho 4
     glBegin(GL_QUADS);
-    //fica faltando um pedaco do triangulo help
-    glColor3d(0.0d, 0.0d, 1.0d);
-    glVertex2d(250, 499);
+    glColor3d(0.0d, 0.0d, 0.0d);
+    glVertex2d(250, 500);
     glColor3d(0.0d, 1.0d, 0.0);
-    glVertex2d(499, 499);
+    glVertex2d(500, 500);
     glColor3d(1.0d, 1.0d, 1.0d);
-    glVertex2d(499, 250);
+    glVertex2d(500, 250);
     glColor3d(1.0d, 0.0d, 0.0d);
     glVertex2d(250, 250);
 
     glEnd();
 }
 void desenha(){
+    //Viewports e esquema pra mudar o fundo
 	glClear(GL_COLOR_BUFFER_BIT);
 	glViewport(0, 250, 250, 250);
 	triangulo();
@@ -71,6 +74,10 @@ void desenha(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glViewport(0, 0, 250, 250);
 	duaslinhas();
+	glScissor(250, 0, 250, 250);
+	glEnable(GL_SCISSOR_TEST);
+	glClearColor(1.0d, 1.0d, 1.0d, 1.0d);
+	glClear(GL_COLOR_BUFFER_BIT);
 	glViewport(250, 0, 250, 250);
 	acolorfulsquare();
 
@@ -78,7 +85,7 @@ void desenha(){
 }
 
 void start(){
-	//Como deixar cada janelinha com a cor própria?
+	//Define o fundo vermelho
 	glClearColor(1.0d, 0.0d, 0.0d, 0.0d);
 	glOrtho(0, WIDTH, 0, HEIGHT, -1, 1);
 }
